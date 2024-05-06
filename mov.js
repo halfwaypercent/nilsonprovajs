60b329bf9a649d0b989759739941d6a09c78b234
halfwaypercent <160871440+halfwaypercent@users.noreply.github.com>
2024-05-06 19:02:17 +0000

    prova

--- mov.js
+++ mov.js
@@ -2,11 +2,11 @@ const canvas = document.getElementById("meuCanvas");
 const ctx = canvas.getContext("2d");
 const Img = new Image();
 let x = 450;
-let y = 10;
+let y = 0;
 const timg = 50;
 
 function moveImg(timestamp){
-  if (x >= 0, y >= 0) {
+  if (x <= 25, y <= 445) {
      x -= 1 ;
      y += 1 ; 
 }
