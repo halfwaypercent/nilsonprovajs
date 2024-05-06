a66525f160f3feeea4ed01806096b1cbf5d288c0
halfwaypercent <160871440+halfwaypercent@users.noreply.github.com>
2024-05-06 10:51:35 +0000

    prova

--- mov.js
+++ mov.js
@@ -1,17 +1,25 @@
 const canvas = document.getElementById("meuCanvas");
 const ctx = canvas.getContext("2d");
 const Img = new Image();
-let rightPos = 1125;
-const imgWidth = 75;
+let x = 450;
+let y = 10;
+const timg = 50;
 
 function moveImg(timestamp){
-  if (rightPos >= 0) {
-     rightPos -= 5;
+  if (x >= 0, y >= 0) {
+     x -= 1 ;
+     y += 1 ; 
 }
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(Img, rightPos, 345, imgWidth, Img.height * (imgWidth / Img.width));
+  ctx.drawImage(Img, x, y, timg, Img.height * (timg / Img.width));
   requestAnimationFrame(moveImg);
 }
-Img.src = "playerZ_red.png";
+
+
+
+
+
+
+Img.src = "aeronave.png";
 
 requestAnimationFrame(moveImg);
